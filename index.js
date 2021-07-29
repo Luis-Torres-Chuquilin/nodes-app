c114a192decc80aef742bb3fbb46423e0eb3998a
Luis Torres Chuquilin <letorres.ch@gmail.com>
2021-07-29 15:45:17 -0500

    First Commit

--- index.js
+++ index.js
@@ -10,13 +10,25 @@ const app = express();
 dotenv.config();
 
 // Middlewre setting up
-// all the routes settled in posts.js will start with  localhost:5000/posts/<route>
+// all the routes settled in ( postRoutes =  posts.js ) will start with  localhost:5000/posts/<route>
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+
+// This is somethign that look when you open api
+app.get("/posts/belong/luist", (req, res) => {
+  res.send("API belong to Luis Torres");
+});
+
+//  Serve static files
+app.use(express.static("client/build"));
+
+app.get("/", (req, res) => {
+  res.sendFile("build", "index.html");
+});
 
 // const CONNECTION_URL =
 //   "mongodb+srv://letorres:letorres@cluster0.4hem8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
