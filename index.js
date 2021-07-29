417db3c5381ad96a1d7e6a1fb9c9cdadbf50e0cc
Luis Torres Chuquilin <letorres.ch@gmail.com>
2021-07-29 13:05:57 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+dotenv.config();
+
+// Middlewre setting up
+// all the routes settled in posts.js will start with  localhost:5000/posts/<route>
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+// const CONNECTION_URL =
+//   "mongodb+srv://letorres:letorres@cluster0.4hem8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
